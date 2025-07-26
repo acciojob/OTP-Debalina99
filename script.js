a65656fa17dbf0432d271441c867e5f1d325104c
Debalina Laha <32841776+Debalina99@users.noreply.github.com>
2025-07-26 20:28:53 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', (e) => {
+    const value = e.target.value;
+
+    if (!/^[0-9]$/.test(value)) {
+      e.target.value = '';
+      return;
+    }
+    if (value && index < inputs.length - 1) {
+      inputs[index + 1].focus();
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    if (e.key === 'Backspace') {
+      if (e.target.value === '' && index > 0) {
+        inputs[index - 1].focus();
+        inputs[index - 1].value = '';
+      }
+    }
+  });
+});
